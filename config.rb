d4132c39970707476e1cecace506c4ec8a130317
Silvan Hagen <silvan@required.ch>
2013-04-01 18:39:32 +0200

    Changed SCSS output

--- config.rb
+++ config.rb
@@ -16,7 +16,7 @@ javascripts_dir = "../required-foundation/javascripts"
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-line_comments = true
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
